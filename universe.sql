2c7f15e023ac9042312bda6db64ed8bddde9b06e
Rafael Vinicius Brambilla Alves <128416211+RafaelViniciusBrambillaAlves@users.noreply.github.com>
2023-05-10 10:12:09 -0300

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -2,6 +2,10 @@
 --Projeto simples de criação de um banco sobre Corpos celestiais 
 --Utilizando o SSMS 
 
+--Excluindo um banco de dados ja existente
+
+DROP DATABASE universe; 
+
 --Criação Banco de Dados Universe
 
 CREATE DATABASE universe; 
@@ -82,61 +86,55 @@ VALUES
 ('s6', 6, 66, 'des6', 'ga6'	, 'name6');
 
 --Inserindo valores ficticios da tabela planet
+ 
+INSERT INTO planet VALUES ('p1', 1, 't1', '1', 1, 1, 's1', 'name1');
+INSERT INTO planet VALUES ('p2', 2, 't2', '2', 1, 0, 's1', 'name2');
+INSERT INTO planet VALUES ('p3', 3, 't3', '3', 1, 0, 's1', 'name3');
+INSERT INTO planet VALUES ('p4', 4, 't4', '4', 0, 1, 's2', 'name4');
+INSERT INTO planet VALUES ('p5', 5, 't5', '5', 1, 0, 's1', 'name5');
+INSERT INTO planet VALUES ('p6', 6, 't6', '6', 0, 1, 's3', 'name6');
+INSERT INTO planet VALUES ('p7', 7, 't7', '7', 0, 1, 's4', 'name7');
+INSERT INTO planet VALUES ('p8', 8, 't8', '8', 1, 0, 's2', 'name8');
+INSERT INTO planet VALUES ('p9', 9, 't9', '9', 0, 1, 's3', 'name9');
+INSERT INTO planet VALUES ('p10', 10, 't10', '10', 1, 1, 's4', 'name10');
+INSERT INTO planet VALUES ('p11', 11, 't11', '11', 1, 1, 's5', 'name11');
+INSERT INTO planet VALUES ('p12', 12, 't12', '12', 0, 0, 's6', 'name12');
 
-INSERT INTO planet(planet_id, size_planet, tips_planet, duration_rotation, potentially_inhabited_planets, have_water, star_id, name)
-VALUES 
-('p1', 1, 't1', '1', 1, 1, 's1', 'name1'),
-('p2', 2, 't2', '2', 1, 0, 's1', 'name2'),
-('p3', 3, 't3', '3', 1, 0, 's1', 'name3'),
-('p4', 4, 't4', '4', 0, 1, 's2', 'name4'),
-('p5', 5, 't5', '5', 1, 0, 's1', 'name5'),
-('p6', 6, 't6', '6', 0, 1, 's3', 'name6'),
-('p7', 7, 't7', '7', 0, 1, 's4', 'name7'),
-('p8', 8, 't8', '8', 1, 0, 's2', 'name8'),
-('p9', 9, 't9', '9', 0, 1, 's3', 'name9'),
-('p10', 10, 't10', '10', 1, 1, 's4', 'name10'),
-('p11', 11, 't11', '11', 1, 1, 's5', 'name11'),
-('p12', 12, 't12', '12', 0, 0, 's6', 'name12');
+-- Inserindo valores ficticios da tabela moon
 
---Inserindo valores ficticios da tabela moon
-
-INSERT INTO moon(moon_id, size_moon, planet_id, duration_rotation, duration_translation, name)
-VALUES 
-('m1', 1, 'p1', 't1', 't11', 'name1'), 
-('m2', 2, 'p1', 't2', 't22', 'name2'), 
-('m3', 3, 'p1', 't3', 't33', 'name3'), 
-('m4', 4, 'p2', 't4', 't44', 'name4'), 
-('m5', 5, 'p2', 't5', 't55', 'name5'), 
-('m6', 6, 'p3', 't6', 't66', 'name6'), 
-('m7', 7, 'p3', 't7', 't77', 'name7'), 
-('m8', 8, 'p4', 't8', 't88', 'name8'), 
-('m9', 9, 'p4', 't9', 't99', 'name9'), 
-('m10', 10, 'p5', 't10', 't1010', 'name10'), 
-('m11', 11, 'p6', 't11', 't1111', 'name11'), 
-('m12', 12, 'p7', 't12', 't1212', 'name12'), 
-('m13', 13, 'p7', 't13', 't1313', 'name13'), 
-('m14', 14, 'p8', 't14', 't1414', 'name14'), 
-('m15', 15, 'p8', 't15', 't1515', 'name15'), 
-('m16', 16, 'p9', 't16', 't1616', 'name16'), 
-('m17', 17, 'p10', 't17', 't1717', 'name17'),
-('m18', 18, 'p10', 't18', 't1818', 'name18'), 
-('m19', 19, 'p11', 't19', 't1919', 'name19'), 
-('m20', 20, 'p12', 't20', 't2020', 'name20');
+INSERT INTO moon VALUES ('m1', 1, 'p1', 't1', 't11', 'name1');
+INSERT INTO moon VALUES ('m2', 2, 'p1', 't2', 't22', 'name2'); 
+INSERT INTO moon VALUES ('m3', 3, 'p1', 't3', 't33', 'name3');
+INSERT INTO moon VALUES ('m4', 4, 'p2', 't4', 't44', 'name4'); 
+INSERT INTO moon VALUES ('m5', 5, 'p2', 't5', 't55', 'name5');
+INSERT INTO moon VALUES ('m6', 6, 'p3', 't6', 't66', 'name6'); 
+INSERT INTO moon VALUES ('m7', 7, 'p3', 't7', 't77', 'name7'); 
+INSERT INTO moon VALUES ('m8', 8, 'p4', 't8', 't88', 'name8'); 
+INSERT INTO moon VALUES ('m9', 9, 'p4', 't9', 't99', 'name9'); 
+INSERT INTO moon VALUES ('m10', 10, 'p5', 't10', 't1010', 'name10'); 
+INSERT INTO moon VALUES ('m11', 11, 'p6', 't11', 't1111', 'name11'); 
+INSERT INTO moon VALUES ('m12', 12, 'p7', 't12', 't1212', 'name12'); 
+INSERT INTO moon VALUES ('m13', 13, 'p7', 't13', 't1313', 'name13'); 
+INSERT INTO moon VALUES ('m14', 14, 'p8', 't14', 't1414', 'name14'); 
+INSERT INTO moon VALUES ('m15', 15, 'p8', 't15', 't1515', 'name15'); 
+INSERT INTO moon VALUES ('m16', 16, 'p9', 't16', 't1616', 'name16'); 
+INSERT INTO moon VALUES ('m17', 17, 'p10', 't17', 't1717', 'name17');
+INSERT INTO moon VALUES ('m18', 18, 'p10', 't18', 't1818', 'name18'); 
+INSERT INTO moon VALUES ('m19', 19, 'p11', 't19', 't1919', 'name19'); 
+INSERT INTO moon VALUES ('m20', 20, 'p12', 't20', 't2020', 'name20');
 
 --Inserindo valores ficticios da tabela constellation 
 
-INSERT INTO constellation(constellations_id, classifications_Constellations, name)
-VALUES	
-('const1', 'class1', 'name1'),
-('const2', 'class2', 'name2'),
-('const3', 'class3', 'name3'),
-('const4', 'class4', 'name4'),
-('const5', 'class5', 'name5'),
-('const6', 'class6', 'name6'),
-('const7', 'class7', 'name7'),
-('const8', 'class8', 'name8'),
-('const9', 'class9', 'name9'),
-('const10', 'class10', 'name10');
+INSERT INTO constellation VALUES ('const1', 'class1', 'name1');
+INSERT INTO constellation VALUES ('const2', 'class2', 'name2');
+INSERT INTO constellation VALUES ('const3', 'class3', 'name3');
+INSERT INTO constellation VALUES ('const4', 'class4', 'name4');
+INSERT INTO constellation VALUES ('const5', 'class5', 'name5');
+INSERT INTO constellation VALUES ('const6', 'class6', 'name6');
+INSERT INTO constellation VALUES ('const7', 'class7', 'name7');
+INSERT INTO constellation VALUES ('const8', 'class8', 'name8');
+INSERT INTO constellation VALUES ('const9', 'class9', 'name9');
+INSERT INTO constellation VALUES ('const10', 'class10', 'name10');
 
 --Referenciando as PRIMARY KEY's
 
